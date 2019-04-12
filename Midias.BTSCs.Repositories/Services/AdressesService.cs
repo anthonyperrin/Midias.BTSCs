0cca30ac87db77657433dc3664e1969985b8a5df
plagnol <antoine.plagnol@gmail.com>
2019-04-12 14:29:18 +0200

    AdressesService et CategoriesService

---
+++ Midias.BTSCs.Repositories/Services/AdressesService.cs
@@ -0,0 +1,128 @@
+﻿using Midias.BTSCs.Dal;
+using Midias.BTSCs.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midias.BTSCs.Services.Services
+{
+
+    public interface IAdressesService
+    {
+        /// <summary>
+        /// Returns the matching adresse
+        /// </summary>
+        /// <param name="id">Adresse Id</param>
+        /// <returns></returns>
+        AdresseDto GetAdresse(int id);
+        /// <summary>
+        /// Returns a list with all the adresses
+        /// </summary>
+        /// <returns></returns>
+        List<AdresseDto> GetAdresses();
+        /// <summary>
+        /// Create a new template of product
+        /// </summary>
+        /// <param name="adresse">Adresse Dto</param>
+        /// <returns></returns>
+        void CreateNewAdresse(AdresseDto adresse);
+        /// <summary>
+        /// Updates the given adresse with its new values
+        /// </summary>
+        /// <param name="adresseDto">New adresse Dto</param>
+        /// <returns></returns>
+        AdresseDto UpdateAdresse(AdresseDto adresseDto);
+        /// <summary>
+        /// Delete the adresse template
+        /// </summary>
+        /// <param name="id">Adresse to remove Id</param>
+        /// <returns></returns>
+        void DeleteAdresse(int id);
+    }
+
+    class AdressesService : ServiceBase, IAdressesService
+    {
+        public AdressesService()
+        {
+        }
+
+        public AdresseDto GetAdresse(int id)
+        {
+            var adresse = Context.Adresse.Where(p => p.Id == id).FirstOrDefault();
+            return new AdresseDto()
+            {
+                Id = adresse.Id,
+                Rue1 = adresse.Rue1,
+                Rue2 = adresse.Rue2,
+                CodePostal = adresse.CodePostal,
+                Ville = adresse.Ville,
+                Pays = adresse.Pays
+            };
+        }
+
+        public List<AdresseDto> GetAdresses()
+        {
+            return Context.Adresse.Select(a => new AdresseDto()
+            {
+                Id = a.Id,
+                Rue1 = a.Rue1,
+                Rue2 = a.Rue2,
+                CodePostal = a.CodePostal,
+                Ville = a.Ville,
+                Pays = a.Pays
+            }).ToList();
+        }
+
+        public void CreateNewAdresse(AdresseDto adresse)
+        {
+            Context.Adresse.Add(new Adresse()
+            {
+                Id = adresse.Id,
+                Rue1 = adresse.Rue1,
+                Rue2 = adresse.Rue2,
+                CodePostal = adresse.CodePostal,
+                Ville = adresse.Ville,
+                Pays = adresse.Pays
+            });
+            Context.SaveChanges();
+        }
+
+        public AdresseDto UpdateAdresse(AdresseDto adresseDto)
+        {
+            var adresse = GetAdresse(adresseDto.Id);
+
+            adresse.Rue1 = adresseDto.Rue1;
+            adresse.Rue2 = adresseDto.Rue2;
+            adresse.CodePostal = adresseDto.CodePostal;
+            adresse.Ville = adresseDto.Ville;
+            adresse.Pays = adresseDto.Pays;
+
+            Context.SaveChanges();
+
+            return new AdresseDto()
+            {
+                Id = adresse.Id,
+                Rue1 = adresse.Rue1,
+                Rue2 = adresse.Rue2,
+                CodePostal = adresse.CodePostal,
+                Ville = adresse.Ville,
+                Pays = adresse.Pays
+            };
+        }
+
+        public void DeleteAdresse(int id)
+        {
+            var adresse = Context.Adresse.Where(p => p.Id == id).FirstOrDefault();
+
+            if (adresse == null)
+                return;
+
+            Context.Adresse.Remove(adresse);
+
+            Context.SaveChanges();
+        }
+    }
+}
---
+++ Midias.BTSCs.Repositories/Services/CategoriesService.cs
@@ -0,0 +1,108 @@
+﻿using Midias.BTSCs.Dal;
+using Midias.BTSCs.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midias.BTSCs.Services.Services
+{
+
+    public interface ICategoriesService
+    {
+        /// <summary>
+        /// Returns the matching categorie
+        /// </summary>
+        /// <param name="id">Categorie Id</param>
+        /// <returns></returns>
+        CategorieDto GetCategorie(int id);
+        /// <summary>
+        /// Returns a list with all the categories
+        /// </summary>
+        /// <returns></returns>
+        List<CategorieDto> GetCategories();
+        /// <summary>
+        /// Create a new template of categorie
+        /// </summary>
+        /// <param name="categorie">Categorie Dto</param>
+        /// <returns></returns>
+        void CreateNewCategorie(CategorieDto categorie);
+        /// <summary>
+        /// Updates the given categorie with its new values
+        /// </summary>
+        /// <param name="categorieDto">New categorie Dto</param>
+        /// <returns></returns>
+        CategorieDto UpdateCategorie(CategorieDto categorieDto);
+        /// <summary>
+        /// Delete the categorie template
+        /// </summary>
+        /// <param name="id">Categorie to remove Id</param>
+        /// <returns></returns>
+        void DeleteCategorie(int id);
+    }
+
+    class CategoriesService : ServiceBase, ICategoriesService
+    {
+        public CategoriesService()
+        {
+        }
+
+        public CategorieDto GetCategorie(int id)
+        {
+            var categorie = Context.Categorie.Where(p => p.Id == id).FirstOrDefault();
+            return new CategorieDto()
+            {
+                Id = categorie.Id,
+                Libelle = categorie.Libelle
+            };
+        }
+
+        public List<CategorieDto> GetCategories()
+        {
+            return Context.Categorie.Select(a => new CategorieDto()
+            {
+                Id = a.Id,
+                Libelle = a.Libelle
+            }).ToList();
+        }
+
+        public void CreateNewCategorie(CategorieDto categorie)
+        {
+            Context.Categorie.Add(new Categorie()
+            {
+                Id = categorie.Id,
+                Libelle = categorie.Libelle
+            });
+            Context.SaveChanges();
+        }
+
+        public CategorieDto UpdateCategorie(CategorieDto categorieDto)
+        {
+            var categorie = GetCategorie(categorieDto.Id);
+
+            categorie.Libelle = categorieDto.Libelle;
+
+            Context.SaveChanges();
+
+            return new CategorieDto()
+            {
+                Id = categorie.Id,
+                Libelle = categorie.Libelle
+            };
+        }
+
+        public void DeleteCategorie(int id)
+        {
+            var categorie = Context.Categorie.Where(p => p.Id == id).FirstOrDefault();
+
+            if (categorie == null)
+                return;
+
+            Context.Categorie.Remove(categorie);
+
+            Context.SaveChanges();
+        }
+    }
+  
+}
